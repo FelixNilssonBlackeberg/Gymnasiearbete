03c92b34953ed4560e3971a23cf84e3f3ab83eb8
PPingWu <82100404+PPingWu@users.noreply.github.com>
2021-04-08 15:49:45 +0200

    Update SEIRvariant.java

--- SEIRvariant.java
+++ SEIRvariant.java
@@ -1,45 +1,59 @@
 package gymnasiearbete;
-
-public class SEIRvariant {
+import java.util.Scanner;
+// Beskriv här vad programmet gör
+public class SIS {
 
     public static void main(String[] args) {
-        
         //Startvärden
+        /*Scanner sc = new Scanner(System.in);
+        System.out.print("Skriv S(0): ");
+        double s = sc.nextDouble();
+        System.out.print("Skriv I(0): ");
+        double i = sc.nextDouble();
+        System.out.print("Skriv β: ");
+        double b = sc.nextDouble();
+        System.out.print("Skriv γ: ");
+        double y = sc.nextDouble();
+        double n = s + i;
+        System.out.print("Skriv antal dagar du vill kolla: ");
+        int dagar = sc.nextInt();*/
+        
         double s = 2000000;
-        double e = 5;
         double i = 5;
-        double r = 0;
         double b = 0.4167;
-        double y = 0.16666667;
-        double a = 0.2;
-        double my = 0.006;
-        double n = s + e + i + r;
-        int dagar = 50;
+        double y = 0.166666667;
+        double n = s + i;
+        int dagar = 100;
         //-------------
+
+        double s_prim;
+
+        double i_prim;
         
-        double s_minskning;
-        double i_prim = 0;
-        double r_okning;
-        double e_prim;
+        double blirsjuka_prim;
         
-        for (int x = 1; x <= dagar; x++) 
-        {
-            s_minskning = (my * n) - (my * s) - (b * i * s)/n;
-            e_prim = ((double)(b* i * s)/n - ((my + a)*e));
-            i_prim = (a * e) - ((y + my)*i);
-            r_okning = (y * i) - (my * r);
+        double i_tot = 0;
+
+
+        for (int x = 1; x <= dagar; x++) {
+            s_prim = ((-b) * i * s)/n + y*i;
             
-            s = (s + s_minskning);
-            e = (e + e_prim);
+            blirsjuka_prim = ((b) * i * s)/n;
+
+            i_prim = ((double)(b * i * s))/n - (y * i);
+
+
+            s = (s + s_prim);
             i = (i + i_prim);
-            r = (r + r_okning);
+            i_tot = (i_tot + blirsjuka_prim);
+
             System.out.println("Utskrift---------------------- " + "DAG " + x);
-            System.out.println("S': " + s_minskning);
-            System.out.println("E: " + e_prim);
+            System.out.println("S': " + s_prim);
             System.out.println("I': " + i_prim);
-            System.out.println("R': " + r_okning);
-            System.out.println(" S: " + s + " E: " + e + " I: " + i + " R: " + r);
+
+            System.out.println("S: " + s + " I: " + i + " I Totalt: " + i_tot);
+            System.out.println();
         }
     }
-    
+
 }
