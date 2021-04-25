7a6b433b0048aef4f9ccf60b3183471eb4427916
PPingWu <82100404+PPingWu@users.noreply.github.com>
2021-04-25 16:31:13 +0200

    Update GrafiskSIR.java

--- GrafiskSIR.java
+++ GrafiskSIR.java
@@ -23,8 +23,10 @@ public class GrafiskSIR extends Application {
         lineChart.setTitle("Coronaviruset, 2020");
         lineChart.setCreateSymbols(false); //Lite störande
         yAxis.setAutoRanging(false);
-        yAxis.setUpperBound(2500000);
-        yAxis.setTickUnit(100000);
+        //yAxis.setUpperBound(2500000);
+        //yAxis.setTickUnit(100000);
+        yAxis.setUpperBound(100000);
+        yAxis.setTickUnit(10000);
         
         //Defining a series
         XYChart.Series series = new XYChart.Series();
@@ -36,20 +38,23 @@ public class GrafiskSIR extends Application {
         XYChart.Series series2 = new XYChart.Series();
         series2.setName("R(t)");
         
+        XYChart.Series series3 = new XYChart.Series();
+        series3.setName("Dödsfall");
+        
         //Startvärden
-        double s = 2500000;
+        double s = 2383265;
         double i = 4;
         double r = 0;
         double n = s + i + r;
         double s_minskning;
         double i_prim = 0;
         double r_ökning;
-        double b = 0.28125;
-        double y = 0.125;
+        double b = 0.415;
+        double y = 0.143;
         
         //Populating the series with data
         //S(t)
-        for(int x = 0; x < 170; x++) //Ändra dagar
+        for(int x = 0; x <= 100; x++) //Ändra dagar
         {
             s_minskning = ((-b) * i * s)/n;
             i_prim = ((double)(b * i * s)/n - (y * i));
@@ -64,10 +69,12 @@ public class GrafiskSIR extends Application {
             series1.getData().add(new XYChart.Data(x, i));
             
             series2.getData().add(new XYChart.Data(x, r));
+            
+            series3.getData().add(new XYChart.Data(x, (r*0.006)));
         }
 
         Scene scene = new Scene(lineChart, 800, 600);
-        lineChart.getData().addAll(series, series1, series2);
+        lineChart.getData().addAll(series, series1, series2, series3);
 
         primaryStage.setTitle("Smittspridningmodeller");
         primaryStage.setScene(scene);
